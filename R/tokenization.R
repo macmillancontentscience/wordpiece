a714a412f24bd25fcf7f6d7399c2a0da316900c8
Jonathan Bratt <jonathan.bratt@macmillan.com>
2020-12-03 14:33:37 -0600

    move tokenization functions from RBERT (with a few modifications)

--- R/tokenization.R
+++ R/tokenization.R
@@ -13,4 +13,478 @@
 # limitations under the License.
 
 
-# tokenize... ----------------------------------------
+# convert_to_unicode ------------------------------------------------------
+
+
+#' Convert `text` to Unicode
+#'
+#' See documentation for `Encoding` for more information.
+#' Assumes utf-8 input.
+#'
+#' @param text character scalar to convert to unicode
+#'
+#' @return input text, converted to unicode if applicable
+#' @export
+#'
+#' @examples
+#' convert_to_unicode("fa\xC3\xA7ile")
+convert_to_unicode <- function(text) {
+    if (validUTF8(text)) { # this seems to work for utf-8 and 'bytes' encodings
+        Encoding(text) <- "UTF-8"
+        return(text)
+    } else {
+        stop("Unsupported string type.")
+    }
+}
+
+
+# load_vocab --------------------------------------------------------------
+
+
+#' Load a vocabulary file
+#'
+#' @param vocab_file path to vocabulary file. File is assumed to be a text file,
+#'   with one token per line, with the line number corresponding to the index of
+#'   that token in the vocabulary. The casedness of the vocabulary is inferred
+#'   and attached as the "is_cased" attribute.
+#'
+#' @return The vocab as a named integer vector. Names are tokens in vocabulary,
+#'   values are integer indices.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{ vocab <- load_vocab(vocab_file = "vocab.txt") }
+load_vocab <- function(vocab_file) {
+    token_list <- readLines(vocab_file)
+    if (length(token_list) == 0) {
+        return(integer(0))
+    }
+    token_list <- purrr::map(token_list, function(token) {
+        convert_to_unicode(trimws(token))})
+    # The vocab is zero-indexed, and we need to preserve that indexing.
+    index_list <- seq_along(token_list) - 1
+    names(index_list) <- token_list
+    # determine casedness of vocab and attach as attribute
+    is_cased <- .infer_case_from_vocab(index_list)
+    attr(index_list, "is_cased") <- is_cased
+    return(index_list)
+}
+
+
+# whitespace_tokenize -----------------------------------------------------
+
+
+#' Run basic whitespace cleaning and splitting on a piece of text.
+#'
+#' @param text Character scalar to tokenize.
+#'
+#' @return Character vector of tokens, split on whitespace.
+#' @export
+#'
+#' @examples
+#' whitespace_tokenize(text = " some\ttext \n with  whitespace ")
+whitespace_tokenize <- function(text) {
+    return(
+        unlist(
+            stringi::stri_split_regex(text, "\\s", omit_empty = TRUE)
+        )
+    )
+}
+
+
+# .strip_accents -----------------------------------------------------------
+
+
+#' Strip accents from a piece of text.
+#'
+#' R implementation of BasicTokenizer._run_strip_accents from
+#' BERT: tokenization.py.
+#'
+#' @param text A character scalar, encoded as utf-8.
+#'
+#' @return text with accents removed.
+#'
+#' @keywords internal
+.strip_accents <- function(text) {
+    text <- stringi::stri_trans_nfd(text)
+
+    return(
+        .apply_to_chars(text,
+                        function(char) {
+                            if(stringi::stri_detect_charclass(char,
+                                                              "\\p{Mn}")) {
+                                return("")
+                            }
+                            return(char)
+                        })
+    )
+}
+
+
+# .split_on_punc -----------------------------------------------------------
+
+
+#' Split text on punctuation.
+#'
+#' (R implementation of BasicTokenizer._run_split_on_punc from
+#' BERT: tokenization.py.)
+#'
+#' @param text A character scalar, encoded as utf-8.
+#'
+#' @return The input text as a character vector, split on punctuation
+#' characters.
+#'
+#' @keywords internal
+.split_on_punc <- function(text) {
+    # this feels icky, but try to break it :-P
+    # Put a unique marker around each punctuation char, then split on the
+    # marker (since we want the punctuation to be included in split).
+    sep_marker <- "a!b"
+    output <- .apply_to_chars(text,
+                              function(char) {
+                                  if(.is_punctuation(char)) {
+                                      return(paste0(sep_marker,
+                                                    char,
+                                                    sep_marker))
+                                  }
+                                  return(char)
+                              })
+    return(
+        unlist(
+            stringi::stri_split_fixed(output, sep_marker, omit_empty = TRUE)
+        )
+    )
+}
+
+
+# .tokenize_chinese_chars --------------------------------------------------
+
+
+#' Add whitespace around any CJK character.
+#'
+#' R implementation of BasicTokenizer._tokenize_chinese_chars from
+#' BERT: tokenization.py. This may result in doubled-up spaces,
+#' but that's the behavior of the Python code.
+#'
+#' @param text A character scalar.
+#'
+#' @return Text with spaces around CJK characters.
+#'
+#' @keywords internal
+.tokenize_chinese_chars <- function(text) {
+    return(
+        .apply_to_chars(text,
+                        function(char) {
+                            cp <- utf8ToInt(char)
+                            if(.is_chinese_char(cp)) {
+                                return(paste0(" ", char, " "))
+                            }
+                            return(char)
+                        })
+    )
+}
+
+
+# .is_chinese_char ---------------------------------------------------------
+
+
+#' Check whether cp is the codepoint of a CJK character.
+#'
+#' R implementation of BasicTokenizer._is_chinese_char from
+#' BERT: tokenization.py. From that file:
+#'  This defines a "chinese character" as anything in the CJK Unicode block:
+#'   https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+#'
+#' Note that the CJK Unicode block is NOT all Japanese and Korean characters,
+#' despite its name. The modern Korean Hangul alphabet is a different block,
+#' as is Japanese Hiragana and Katakana. Those alphabets are used to write
+#' space-separated words, so they are not treated specially and are handled
+#' like the alphabets of the other languages.
+#'
+#' @param cp A unicode codepoint, as an integer.
+#'
+#' @return Logical TRUE if cp is codepoint of a CJK character.
+#'
+#' @keywords internal
+.is_chinese_char <- function(cp) {
+    if ((cp >= 0x4E00 & cp <= 0x9FFF) |
+        (cp >= 0x3400 & cp <= 0x4DBF) |
+        (cp >= 0x20000 & cp <= 0x2A6DF) |
+        (cp >= 0x2A700 & cp <= 0x2B73F) |
+        (cp >= 0x2B740 & cp <= 0x2B81F) |
+        (cp >= 0x2B820 & cp <= 0x2CEAF) |
+        (cp >= 0xF900 & cp <= 0xFAFF) |
+        (cp >= 0x2F800 & cp <= 0x2FA1F)) {
+        return(TRUE)
+    }
+    return(FALSE)
+}
+
+
+# .clean_text --------------------------------------------------------------
+
+
+#' Perform invalid character removal and whitespace cleanup on text.
+#'
+#' (R implementation of BasicTokenizer._clean_text from
+#' BERT: tokenization.py.)
+#'
+#' @param text A character scalar.
+#'
+#' @return Cleaned up text.
+#'
+#' @keywords internal
+.clean_text <- function(text) {
+    return(
+        .apply_to_chars(text,
+                       function(char) {
+                           cp <- utf8ToInt(char)
+                           if (cp == 0 | cp == 0xfffd | .is_control(char)) {
+                               return("")
+                           } else if (.is_whitespace(char)) {
+                               return(" ")
+                           }
+                           return(char)
+                       })
+    )
+}
+
+
+# tokenize_word -----------------------------------------------------------
+
+
+#' Tokenize a single "word" (no whitespace).
+#'
+#' In BERT: tokenization.py,
+#' this code is inside the tokenize method for WordpieceTokenizer objects.
+#' I've moved it into its own function for clarity.
+#' Punctuation should already have been removed from the word.
+#'
+#' @param word Word to tokenize.
+#' @param vocab Character vector containing vocabulary words
+#' @param unk_token Token to represent unknown words.
+#' @param max_chars Maximum length of word recognized.
+#'
+#' @return Input word as a list of tokens.
+#' @export
+#'
+#' @examples
+#' tokenize_word("unknown", vocab = c("un" = 0, "##known" = 1))
+#' tokenize_word("known", vocab = c("un" = 0, "##known" = 1))
+tokenize_word <- function(word, vocab, unk_token = "[UNK]", max_chars = 100) {
+    vocab <- names(vocab) # quick patch for now.
+    if (stringi::stri_length(word) > max_chars) {
+        return(unk_token)
+    }
+    if (word %in% vocab) {
+        return(word)
+    }
+
+    is_bad  <- FALSE
+    start <- 1
+    sub_tokens <- character(0)
+    while (start <= stringi::stri_length(word)) {
+        end <- stringi::stri_length(word)
+
+        cur_substr  <- NA_character_
+        while (start <= end) {
+            sub_str <- substr(word, start, end)   # inclusive on both ends
+            if (start > 1) {  # means this substring is a suffix, so add '##'
+                sub_str <- paste0("##", sub_str)
+            }
+            if (sub_str %in% vocab) {
+                cur_substr <- sub_str
+                break
+            }
+            end <- end - 1
+        }
+        if (is.na(cur_substr) ) {
+            is_bad <-  TRUE
+            break
+        }
+
+        sub_tokens <- append(sub_tokens, cur_substr)
+        start <- end + 1 # pick up where we left off
+    }
+
+    if (is_bad) {
+        return(unk_token)
+    }
+    return(sub_tokens)
+}
+
+
+# .is_whitespace -----------------------------------------------------------
+
+
+#' Check whether `char` is a whitespace character
+#'
+#' R implementation of _is_whitespace from BERT: tokenization.py.
+#'
+#' "\\t", "\\n", and "\\r" are technically control characters but we treat them
+#' as whitespace since they are generally considered as such.
+#'
+#' @param char A character scalar, comprising a single unicode character.
+#'
+#' @return TRUE if char is a whitespace character.
+#'
+#' @keywords internal
+#' @md
+.is_whitespace <- function(char) {
+    # This is a way to check the unicode general category:
+    # stringi::stri_detect_charclass(char, "\\p{Zs}")
+    # Maybe enforce that char is *single* character here?
+
+    if (char %in% c(" ", "\t", "\n", "\r")) {
+        return(TRUE)
+    }
+    return(stringi::stri_detect_charclass(char, "\\p{Zs}"))
+}
+
+# .is_control --------------------------------------------------------------
+
+
+#' Check whether `char` is a control character.
+#'
+#' R implementation of _is_control from BERT: tokenization.py.
+#'
+#' "\\t", "\\n", and "\\r" are technically control characters but we treat them
+#' as whitespace since they are generally considered as such.
+#' @param char A character scalar, comprising a single unicode character.
+#'
+#' @return TRUE if char is a control character.
+#'
+#' @keywords internal
+#' @md
+.is_control <- function(char) {
+    # Maybe enforce that char is *single* character here?
+
+    if (char %in% c(" ", "\t", "\n", "\r")) {
+        return(FALSE)
+    }
+    return(stringi::stri_detect_charclass(char, "\\p{C}"))
+}
+
+# .is_punctuation ----------------------------------------------------------
+
+
+#' Check whether `char` is a punctuation character.
+#'
+#' R implementation of _is_punctuation from BERT: tokenization.py.
+#'
+#' We treat all non-letter/number ASCII as punctuation.
+#' Characters such as "^", "$", and "`" are not in the Unicode
+#' Punctuation class but we treat them as punctuation anyway, for
+#' consistency.
+#' @param char A character scalar, comprising a single unicode character.
+#'
+#' @return TRUE if char is a punctuation character.
+#'
+#' @keywords internal
+#' @md
+.is_punctuation <- function(char) {
+    cp <- utf8ToInt(char)
+    if ((cp >= 33 & cp <= 47) | (cp >= 58 & cp <= 64) |
+        (cp >= 91 & cp <= 96) | (cp >= 123 & cp <= 126)) {
+        return(TRUE)
+    }
+    return(stringi::stri_detect_charclass(char, "\\p{P}"))
+}
+
+
+# .apply_to_chars ----------------------------------------------------------
+
+
+#' Apply a function to each character in a string.
+#'
+#' Utility function for something done a lot in this package.
+#'
+#' @param text A character scalar to process.
+#' @param .f The function to apply to each character. Should return a character
+#' scalar, given a single-character input.
+#' @param ... Other arguments to pass to .f.
+#'
+#' @return The character scalar obtained by applying the given function to
+#' each character of the input string, and concatenating the results.
+#'
+#' @keywords internal
+.apply_to_chars <- function(text, .f, ...) {
+    paste(
+        purrr::map_chr(unlist(strsplit(text, "")), .f, ...),
+        collapse = ""
+    )
+}
+
+# tokenize_single_sequence ----------------------------------------------------
+
+#' Tokenize Sequence with Word Pieces
+#'
+#' Given a single sequence of text and a wordpiece vocabulary, tokenizes the
+#' text.
+#'
+#' @inheritParams tokenize_word
+#' @param text Character scalar; text to tokenize.
+#' @param vocab The word piece vocabulary to use.
+#'
+#' @return An named integer vector, giving the tokenization of the input
+#'   sequence. The integers values are the token ids, and the names are the
+#'   tokens.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' vocab <- load_vocab("path/to/vocab.txt")
+#' tokens <- tokenize_single_sequence(
+#'   text = "I love tacos!",
+#'   vocab = vocab
+#' )
+#' }
+tokenize_single_sequence <- function(text,
+                                     vocab,
+                                     unk_token = "[UNK]",
+                                     max_chars = 100) {
+    is_cased <- attr(vocab, "is_cased")
+    if (!is_cased) {
+        text <- tolower(text)
+    }
+
+    text <- convert_to_unicode(text)
+    text <- .clean_text(text)
+    text <- .tokenize_chinese_chars(text)
+    text <- .strip_accents(text)
+    text <- .split_on_punc(text)
+    text <- purrr::map(whitespace_tokenize(text),
+                       # tokenize_word is the part we'll modify in morphemizer.
+                       .f = tokenize_word,
+                       vocab = vocab,
+                       unk_token = unk_token,
+                       max_chars = max_chars)
+    text <- unlist(text)
+    ids <- vocab[text]
+    names(ids) <- text
+    return(ids)
+}
+
+
+# .infer_case_from_vocab --------------------------------------------------
+
+#' Determine Vocabulary Casedness
+#'
+#' Determine whether or not a wordpiece vocabulary is case-sensitve.
+#'
+#' If none of the tokens in the vocabulary start with a capital letter, it will
+#' be assumed to be uncased. Note that tokens like "[CLS]" contain uppercase
+#' letters, but don't start with uppercase letters.
+#'
+#' @param vocab The vocabulary as a named integer vector.
+#' @return TRUE if the vocabulary is cased, FALSE if uncased.
+#'
+#' @keywords internal
+.infer_case_from_vocab <- function(vocab) {
+    is_cased <- any(grepl(pattern = "^[A-Z]", names(vocab)))
+    is_cased <- any(stringi::stri_detect(str = names(vocab), regex = "^[A-Z]"))
+    return(is_cased)
+}
+
+
